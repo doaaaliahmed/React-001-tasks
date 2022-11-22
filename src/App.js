d25ad9b37415a0dc721fea86459016f58fbbc60c
doaaaliahmed <doaaali3@yahoo.com>
2022-11-22 05:07:27 +0200

    first-react-task

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [list, setList] = useState([]);
+  const [text, setText] = useState("");
+
+  const enteredTextHandler = (e) => setText(e.target.value);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (text.trim() === "") return;
+
+    setList((prevItem) => [...prevItem, text]);
+    setText("");
+  };
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <div className="form-control">
+          <input type="text" onChange={enteredTextHandler} value={text} />
+          <button>Submit</button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <ul>
+          {list.map((l) => (
+            <li>{l}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default App;
